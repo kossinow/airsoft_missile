76d076774ca9d7a78d4a204ab3ecfb6ad52c8710
Roman Kossinov <kossinow@gmail.com>
2024-09-20 16:44:45 +0500

    remove settings, added logic

--- antenna/antenna.ino
+++ antenna/antenna.ino
@@ -5,13 +5,11 @@
 
 #include <SPI.h>
 #include "RF24.h"
-#include <Button.h>
-#include <EEPROM.h>
 
 // D2-D4 контакты азимутов
-#define azimuth_1_pin  2
-#define azimuth_2_pin  3
-#define azimuth_3_pin  4
+#define azimuth_1  2
+#define azimuth_2  3
+#define azimuth_3  4
 
 // D5 контакт электроподжига
 #define fire_pin 5
@@ -19,106 +17,60 @@
 // D6 светодиод
 #define led 6
 
-// D7 кнопка настройки
-#define button_pin 7
-
 // D9-D13 заняты радиомодулем
 # define CLI 9
 # define CLO 10
 
 RF24 radio(9, 10);  // "создать" модуль на пинах 9 и 10
-Button button(button_pin);
-
-byte mode = 0; // флаг режимов
-long last_tick; // отсчет веремени мигания
-int target_azimuth; // азимут цели
-int blinking_amount; // обратный счтечик для мигания настроек 
-bool led_state = false;
-bool released = false; // убеждаемся что кнопка отжата
 
 byte address[][6] = {"1Node", "2Node"}; //возможные номера труб
-bool payload = true;
+int pin_condition[3];
+int message;
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(led, OUTPUT);
   pinMode(fire_pin, OUTPUT);
-  pinMode(azimuth_1_pin, INPUT_PULLUP);
-  pinMode(azimuth_2_pin, INPUT_PULLUP);
-  pinMode(azimuth_3_pin, INPUT_PULLUP);
-  button.begin();
+  pinMode(azimuth_1, INPUT_PULLUP);
+  pinMode(azimuth_2, INPUT_PULLUP);
+  pinMode(azimuth_3, INPUT_PULLUP);
 
-  target_azimuth = EEPROM.read(1); // считываем азимут цели
-
-  if (digitalRead(button_pin)) { // вход в режим настройки
-    mode = 1;
+  while (!radio.begin()) {// ждем инициализацию радио
+    Serial.println("radio hardware is not responding!!");
+    delay(50);
+    digitalWrite(led, 0);
+    delay(50);
     digitalWrite(led, 1);
-    Serial.println("settings");
-    last_tick = millis();
   }
-  else {
-    mode = 0;
-    while (!radio.begin()) {// ждем инициализацию радио
-      Serial.println("radio hardware is not responding!!");
-      delay(100);
-    }
-    radio.setPALevel (RF24_PA_LOW); // уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
-    radio.setPayloadSize(sizeof(payload)); // размер пакета, в байтах 4
-    radio.openReadingPipe(1, address[0]);   // хотим слушать трубу 0
-    radio.startListening();  // слушаем радиоэфир, мы приемник
-
-  }
+  radio.setPALevel (RF24_PA_LOW); // уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.setPayloadSize(sizeof(pin_condition)); // размер пакета, в байтах 4
+  radio.openReadingPipe(1, address[0]);   // хотим слушать трубу 0
+  radio.startListening();  // слушаем радиоэфир, мы приемник
 
 }
 
 void loop() {
+  byte pipeNo;
+  while (radio.available(&pipeNo)) {        // слушаем эфир со всех труб
+    radio.read(&message, sizeof(message));  // чиатем входящий сигнал
 
-  switch (mode) { // главное меню - переключение между состояниями
-    case 0: standby(); break;
-    case 1: settings(); break;
-  }
-}
-
-void standby() { // режим работы
-
-}
-
-void settings() { // режим настройки цели
-
-  if (button.released() & released){ // изменение количества попаданий
-    target_azimuth ++;
-    if (target_azimuth > 3){
-      target_azimuth = 1;
+    if (message == 1) { // если получили 1, отправляем состояние пинов азимутов
+      digitalWrite(led, 0);
+      bool pin_condition[3] = {digitalRead(azimuth_1), digitalRead(azimuth_2), digitalRead(azimuth_3)};
+      // переходим в режим вещания
+      radio.write(&pin_condition, sizeof(pin_condition));
+      // слушаем
+      digitalWrite(led, 1);
+      message = 0;
     }
-    EEPROM.write(1, target_azimuth);
-    last_tick = millis();
-    blinking_amount = target_azimuth;
-    led_state = false;
-    Serial.println(target_azimuth);
-  }
-
-  if (blinking_amount > 0){ // мигание показывающее количество необходимых попаданий
-    digitalWrite(led, led_state);
-    if (millis() - last_tick > 400) {
-      last_tick = millis();
-      if (!led_state) {
-        led_state = true;
-      }
-      else {
-        led_state = false;
-        blinking_amount --;
-      } 
+    else if (message == 2) { // если получили 2, запускаем ракеты
+      digitalWrite(fire_pin, 1);
+      delay(2000);
+      digitalWrite(fire_pin, 0);
+      message = 0;
     }
   }
-  else {
-    led_state = false;
-    if (millis() - last_tick > 1000){
-      last_tick = millis();
-      blinking_amount = target_azimuth;
-      released = true;
-    }
-  }
+
+
 }
-
-
